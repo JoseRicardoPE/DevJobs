a4f75b3b081d08d4a0aa424324b50237f4b84fe2
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2022-08-25 12:23:24 -0500

    Agregando variables de entorno

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const exphbs = require("express-handlebars")
 const router = require("./routes/routes.js");
 
+require("dotenv").config({path: "vars.env"})
+
 const app = express();
 
 // * Habilitar handlebars como view
@@ -13,7 +15,7 @@ app.use(express.static("public"));
 
 app.use("/", router());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`http://localhost:${port}/ Server is listening on port ${port}`)
 });
